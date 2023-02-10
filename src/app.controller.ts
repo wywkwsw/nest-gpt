48cb6e4e7ea7fd0444c4b2835c293433ae439be0
yangjunhao <yangjh1@jiguang.cn>
2023-02-10 18:42:41 +0800

    🎉 init: 初始化nest项目搭建

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post } from "@nestjs/common";
+import { AppService } from "./app.service";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  @Post()
+  create(): string {
+    return "This action adds a new cat";
+  }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
